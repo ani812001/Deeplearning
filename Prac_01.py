ec8b2b8bf2786c58bc15384e3dcabd0a4c1cd776
aniket kumar <77187170+ani812001@users.noreply.github.com>
2025-08-06 13:00:02 +0530

    Update and rename Prac_01 to Prac_01.py

--- Prac_01.py
+++ Prac_01.py
@@ -2,3 +2,7 @@
 from keras.datasets import mnist
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 _____________________________________________________________________________________________
+print(train_images.shape)
+my_slice = train_images[10]
+print(my_slice.shape) # select the tenth image from the dataset of 60K Image 
+____________________________________________________________________________________________________
