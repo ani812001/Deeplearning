3009252f2673799d234c6a75a656b9936e8e5f3e
aniket kumar <77187170+ani812001@users.noreply.github.com>
2025-08-06 13:03:00 +0530

    Update Prac_01.py

--- Prac_01.py
+++ Prac_01.py
@@ -6,3 +6,10 @@ print(train_images.shape)
 my_slice = train_images[10]
 print(my_slice.shape) # select the tenth image from the dataset of 60K Image 
 ____________________________________________________________________________________________________
+my_slice = train_images[10:100]
+print(my_slice.shape) # select the tenth image from the dataset of 60K Image
+__________________________________________________________________________________________
+
+my_slice = train_images[10:100, 2:28 , 0:28]
+print(my_slice.shape) # select the tenth image from the dataset of 60K Image
+______________________________________________________________________________________________
