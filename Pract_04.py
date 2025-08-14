17c0d7d6db8af570e4572f0815f8b7f6fa832f6e
aniket kumar <77187170+ani812001@users.noreply.github.com>
2025-08-14 15:19:39 +0530

    Update Pract_04.py

--- Pract_04.py
+++ Pract_04.py
@@ -19,3 +19,71 @@ ________________________________________________________________________________
 
 
 
+import numpy as np
+
+# Sigmoid activation function
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+# Derivative of sigmoid
+def sigmoid_derivative(x):
+    return x * (1 - x)
+
+# Input dataset (XOR truth table)
+x = np.array([[0, 0],
+              [0, 1],
+              [1, 0],
+              [1, 1]])
+
+# Output labels
+y = np.array([[0],
+              [1],
+              [1],
+              [0]])
+
+# Seed for reproducibility
+np.random.seed(42)
+
+# Define network architecture
+input_layer_neurons = 2
+hidden_layer_neurons = 2
+output_neurons = 1
+
+# Random weights and biases
+weights_hidden = np.random.uniform(size=(input_layer_neurons, hidden_layer_neurons))
+bias_hidden = np.random.uniform(size=(1, hidden_layer_neurons))
+weights_output = np.random.uniform(size=(hidden_layer_neurons, output_neurons))
+bias_output = np.random.uniform(size=(1, output_neurons))
+
+# Training parameters
+learning_rate = 0.1
+
+for epoch in range(10000):
+
+  hidden_layer_input = np.dot(x, weights_hidden) + bias_hidden
+  hidden_layer_output = sigmoid(hidden_layer_input)
+
+  final_input = np.dot(x, weights_output) + bias_output
+  final_output = sigmoid(final_input)
+
+  #error
+  error = y - final_output
+
+  # Backpropagation
+  d_output = error * sigmoid_derivative(final_output)
+  error_hidden = d_output.dot(weights_output.T)
+  d_hidden = error_hidden * sigmoid_derivative(hidden_layer_output)
+
+  # Update weights and biases
+  weights_output += hidden_layer_output.T.dot(d_output) * learning_rate
+  bias_output += np.sum(d_output, axis=0, keepdims=True) * learning_rate
+  weights_hidden += x.T.dot(d_hidden) * learning_rate
+  bias_hidden += np.sum(d_hidden, axis=0, keepdims=True) * learning_rate
+
+#final Output
+print("Final predictions after training :")
+print(np.round(final_output))
+
+
+____________________________________________________________________________________________________________________
+
