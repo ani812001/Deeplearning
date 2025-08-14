c181f77e6b657c5fce375d6d9f0d6cb285dfc93a
aniket kumar <77187170+ani812001@users.noreply.github.com>
2025-08-14 15:39:22 +0530

    Update Pract_04.py

--- Pract_04.py
+++ Pract_04.py
@@ -101,3 +101,28 @@ plt.show()
 
 ____________________________________________________________________________________________________________________
 
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import numpy as np
+
+X = np.array([[0,0], [0,1], [1,0], [1,1]])
+y = np.array([[0], [1], [1], [0]])
+
+model = Sequential()
+model.add(Dense(4, input_dim=2, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(X, y, epochs=1000, verbose=0)
+
+print("Final predictions after training:")
+predictions = model.predict(X)
+print(np.round(predictions))
+
+
+__________________________________________________________________________________________________________________________
+
+
+
